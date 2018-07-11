c4a5f51b2d5bf845ad48b11fdcc695dbd2fa1b18
katiejg <jng.katie@gmail.com>
2018-07-11 14:44:26 +0000

    Done.

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,30 @@
-
 # Code your methods below
+
+def base_footwork
+  puts "Step 1: Lift right foot."
+  puts "Step 2: Return right foot to the ground."
+  puts "Step 3: Finishing with a small skip-step."
+end
+def starting_stance
+  puts "Plant legs far apart."
+end
+def bob_the_reins
+  puts "Do Step 1: Holding your arms out in front of you, straight and at chest level"
+  puts "Step 2: Cross your right wrist over your left and hold them together"
+  puts  "Step 3: Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end
+def lasso 
+  puts "Do Step 1: Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "Step 2: Lift right arm up and point right elbow diagonally to the right."
+  puts "Step 3: Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end
+def skip_step(foot)
+  puts "Briefly, Lower #{foot} foot to the ground."
+  puts "Bounce #{foot} foot back up slightly, kicking it a few inches back."
+  puts "#{foot}, Right, #{foot} and #{foot}"
+end
+def bust_a_move
+  puts "Be cool"
+  puts "Be snazzy!"
+  puts "Be cool and be snazzy"
+end
